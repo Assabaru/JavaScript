b7297e45e34c91589515bf4f987f095ad9d57496
Assabaru <hhhakash24@gmail.com>
2025-03-29 15:00:05 +0600

    Add files via upload

---
+++ Math.js
@@ -0,0 +1,25 @@
+console.log(Math.PI)
+
+var n = (4.77)
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+console.log(Math.max(100,200,700))
+console.log(Math.min(99,54,67))
+console.log(Math.pow(4,3))
+
+
+
+
+
+//js date
+var date = new Date()
+console.log(date)
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getHours())
+console.log(date.getMinutes())
---
+++ if esle.js
@@ -0,0 +1,62 @@
+
+
+var  score =75
+
+    if (score >= 80) {
+        console.log("Your grade is: A+");
+    } else if (score >= 75) {
+        console.log("Your grade is: A");
+    } else if (score >= 70) {
+        console.log("Your grade is: A-");
+    } else if (score >= 65) {
+        console.log("Your grade is: B+");
+    } else if (score >= 60) {
+        console.log("Your grade is: B");
+    } else if (score >= 55) {
+        console.log("Your grade is: B-");
+    } else if (score >= 50) {
+        console.log("Your grade is: C+");
+    } else if (score >= 45) {
+        console.log("Your grade is: C");
+    } else if (score >= 40) {
+        console.log("Your grade is: D");
+    } else if (score >= 0) {
+        console.log("Your grade is: F");
+    } else {
+        console.log("Invalid score");
+    }
+
+
+
+
+
+
+
+    var  score =65
+        if (score >= 80 && score <= 100) {
+            console.log("Your grade is: A+");
+        } else if (score >= 75 && score <= 79) {
+            console.log("Your grade is: A");
+        } else if (score >= 70 && score <= 74) {
+            console.log("Your grade is: A-");
+        } else if (score >= 65 && score <= 69) {
+            console.log("Your grade is: B+");
+        } else if (score >= 60 && score <= 64) {
+            console.log("Your grade is: B");
+        } else if (score >= 55 && score <= 59) {
+            console.log("Your grade is: B-");
+        } else if (score >= 50 && score <= 54) {
+            console.log("Your grade is: C+");
+        } else if (score >= 45 && score <= 49) {
+            console.log("Your grade is: C");
+        } else if (score >= 40 && score <= 44) {
+            console.log("Your grade is: D");
+        } else if (score >= 0 && score < 40) {
+            console.log("Your grade is: F");
+        } else {
+            console.log(" Invalid");
+        }
+    
+
+    
+    
