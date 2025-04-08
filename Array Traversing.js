46379165d254505038d9324479a6e3a6a435c635
Assabaru <hhhakash24@gmail.com>
2025-04-09 00:52:54 +0600

    Day2

---
+++ Array Traversing.js
@@ -0,0 +1,7 @@
+var arr=[4,5,6,7,8,9,10]
+for(var i=0; i<arr.length; i++){
+    //console.log(arr[i])
+    arr[i] = arr[i] + 1     // Incrementing each element by 1
+    console.log(arr[i])
+}
+console.log(arr)
