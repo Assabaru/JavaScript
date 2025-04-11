81a8466c25b96ca38c16f29f15bc58319176ac9b
Assabaru <hhhakash24@gmail.com>
2025-04-11 22:38:24 +0600

    Day 3

---
+++ Array I/object assign.js
@@ -0,0 +1,17 @@
+var point = {
+  x: 10,    // x coordinate
+  y: 20     // y coordinate 
+};
+point.z = 30; 
+console.log(point);
+
+
+
+//constructor function
+var obj = object ()
+object.a = 10
+console.log(object)
+var object2 = new object()
+object2.a = 30
+console.log(object2)
+ 
